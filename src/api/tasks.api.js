8bb1269f7fb42109fb42ef1fc2f1568ff7b053bb
alvaro <alvaro.apariciofdz@gmail.com>
2024-02-03 13:45:22 +0100

    refactorized code by filtering tasks directly in the getAllTasks api function

--- src/api/tasks.api.js
+++ src/api/tasks.api.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
+import { STATE_CHOICES } from '../consts';
 
 const tasksApi = axios.create({
   baseURL: 'http://localhost:8000/tasks/api/v1/tasks/',
 });
 
-export const getAllTasks = () => tasksApi.get('/');
+export const getAllTasks = () => {
+  return tasksApi.get('/').then(({ data }) => {
+    return {
+      pending: data.filter((task) => task.state === STATE_CHOICES.pending),
+      doing: data.filter((task) => task.state === STATE_CHOICES.doing),
+      done: data.filter((task) => task.state === STATE_CHOICES.done),
+    };
+  });
+};
 export const getTask = (id) => tasksApi.get(`/${id}/`);
 export const createTask = (task) => tasksApi.post('/', task);
 export const deleteTask = (id) => tasksApi.delete(`/${id}`);
--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -4,35 +4,16 @@ import { getAllTasks } from '../api/tasks.api';
 import { STATE_CHOICES } from '../consts';
 
 const initialState = {
-  pending: { label: STATE_CHOICES.pending, tasks: [] },
-  doing: { label: STATE_CHOICES.doing, tasks: [] },
-  done: { label: STATE_CHOICES.done, tasks: [] },
+  [STATE_CHOICES.pending]: [],
+  [STATE_CHOICES.doing]: [],
+  [STATE_CHOICES.done]: [],
 };
 
 export function TasksPage() {
   const [tasks, setTasks] = useState(initialState);
   const [isDragging, setIsDragging] = useState(false);
-
   const handleDragging = (dragging) => setIsDragging(dragging);
-
-  async function loadTasks() {
-    const { data } = await getAllTasks();
-
-    setTasks({
-      pending: {
-        ...tasks.pending,
-        tasks: data.filter((task) => task.state === STATE_CHOICES.pending),
-      },
-      doing: {
-        ...tasks.doing,
-        tasks: data.filter((task) => task.state === STATE_CHOICES.doing),
-      },
-      done: {
-        ...tasks.done,
-        tasks: data.filter((task) => task.state === STATE_CHOICES.done),
-      },
-    });
-  }
+  const loadTasks = async () => setTasks(await getAllTasks());
 
   useEffect(() => {
     loadTasks();
@@ -40,13 +21,13 @@ export function TasksPage() {
 
   return (
     <div className='grid grid-cols-3 gap-5'>
-      {Object.keys(tasks).map((taskState) => (
+      {Object.entries(tasks).map(([taskState, tasksData]) => (
         <div key={taskState} className='p-3'>
           <h1 className='text-center font-bold mb-3'>
-            {tasks[taskState].label.toUpperCase()}
+            {taskState.toUpperCase()}
           </h1>
           <TasksContainer
-            tasks={tasks[taskState].tasks}
+            tasks={tasksData}
             state={taskState.toLowerCase()}
             isDragging={isDragging}
             handleDragging={handleDragging}
